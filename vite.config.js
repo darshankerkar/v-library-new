c01c61a88272684d228609f07b0972f8e620d5c5
darshankerkar <darshankerkar09@gmail.com>
2025-07-10 00:05:09 +0530

    Commit with CRLF warning again

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'; 
 
 export default defineConfig({
-  base: '/v-library-new/', // <-- Add this line for GitHub Pages
+  base: '/', // <-- Add this line for GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
